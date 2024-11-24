caa31623e59786602c5288c884dee0c8f6cf5e60
jokerSharp <qasoftware@tutanota.com>
2024-11-24 20:18:19 +0300

    add getWithMeals() methods in ProfileRestController and AdminRestController

--- src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminRestController.java
@@ -56,4 +56,9 @@ public class AdminRestController extends AbstractUserController {
     public User getByMail(@RequestParam String email) {
         return super.getByMail(email);
     }
+
+    @GetMapping("/with-meals")
+    public User getWithMeals(@RequestParam int id) {
+        return service.getWithMeals(id);
+    }
 }
